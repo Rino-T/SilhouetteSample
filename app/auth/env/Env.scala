cd3f35804ff6b78493de137e8a090ab5203ea44d
千葉俊典 <toshinori.chiba.p@gmail.com>
2020-12-29 10:06:02 +0900

    application.conf の編集

---
+++ app/auth/env/Env.scala
@@ -0,0 +1,10 @@
+package auth.env
+
+import com.mohiva.play.silhouette.api.Env
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import models.User
+
+trait CookieEnv extends Env {
+  override type I = User
+  override type A = CookieAuthenticator
+}
---
+++ app/modules/BaseModule.scala
@@ -0,0 +1,8 @@
+package modules
+
+import com.google.inject.AbstractModule
+import net.codingwell.scalaguice.ScalaModule
+
+class BaseModule extends AbstractModule with ScalaModule {
+  override def configure(): Unit = {}
+}
